dadff4020d40b633470c1f9cc57a49bee6ee94f8
Victor Sanborn <victor.sanborn@hogia.se>
2022-10-17 14:30:55 +0200

    api

---
+++ pages/api/user-information/victor/[information].tsx
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "../../../../Models/user";
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+    //http://localhost:3000/api/user-information/victor/WubalubaDubDub
+
+    // Query for GET
+    console.log("req", req.query)
+
+    // Body for POST
+    // console.log("req", req.body)
+
+    let userInfo: User = new User;
+    userInfo.UserId = 1;
+    userInfo.UserName = "King Virr";
+    userInfo.UserTitle = "The Royal Way"
+    userInfo.Information = req.query.information.toString()
+
+	res.status(200).json(userInfo);
+}
